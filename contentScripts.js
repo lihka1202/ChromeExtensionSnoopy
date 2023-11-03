58c1829bd0f70f30b42997dd1a65fcc887b968f0
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-11-03 10:57:26 +0800

    feat : Changes propogated through storage

--- contentScripts.js
+++ contentScripts.js
@@ -1,6 +1,21 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.url) {
-        // Handle the message from background.js
-        console.log(message.url);
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     if (message.url) {
+//         // Handle the message from background.js
+//         console.log(message.url);
+//     }
+// });
+let currentURL = ""
+chrome.storage.onChanged.addListener(function(changes, areaName) { //changes --> Key, areaname --> type of storage
+    for (const key in changes) {
+        if (Object.hasOwnProperty.call(changes, key)) {
+            const change = changes[key];
+            console.log(`Storage key '${key}' in area '${areaName}' changed.`);
+            console.log(`Old value: ${change.oldValue}`);
+            console.log(`New value: ${change.newValue}`);
+            currentURL = change.newValue
+            console.log("CurrentURL Value changed to: ", currentURL)
+        }
     }
 });
+
+console.log(currentURL)
--- popup.js
+++ popup.js
@@ -2,12 +2,18 @@
 //Get the data from the popup.html
 const urlHolder = document.getElementById("url")
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.url) {
-      // Handle the message from background.js
-      console.log(message.url);
-      urlHolder.textContent = message.url
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  if (tabs.length > 0) {
+      const currentURL = tabs[0].url;
+      urlHolder.textContent = currentURL
+      console.log("Current URL: " + currentURL)
+
+      chrome.storage.local.set({
+        currentURL : currentURL
+      }, ()=>{
+        console.log("URL has been set in the chrome storage")
+      })
   }
 });
-
 
