e33bc3059e734d8ac5229a7d852ead92238cd1db
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-11-05 16:35:55 +0800

    feat! : Backend works! Occurence tracker included

--- contentScripts.js
+++ contentScripts.js
@@ -10,17 +10,21 @@ chrome.storage.onChanged.addListener(function(changes, areaName) { //changes -->
         if (Object.hasOwnProperty.call(changes, key)) {
             const change = changes[key];
 
-            if(key === 'currentURL') // Strict Target matching needed
-            console.log(`Storage key '${key}' in area '${areaName}' changed.`);
-            console.log(`Old value: ${change.oldValue}`);
-            console.log(`New value: ${change.newValue}`);
-            currentURL = change.newValue
-            console.log("CurrentURL Value changed to: ", currentURL)
-            console.log("URL has been set in the chrome storage")
-            console.log("websites array has been set in the chrome storage")
-            console.log("table array has been set in the chrome storage")
-
-            console.log()
+            if(key === 'currentURL') {
+                console.log(`Storage key '${key}' in area '${areaName}' changed.`);
+                console.log(`Old value: ${change.oldValue}`);
+                console.log(`New value: ${change.newValue}`);
+                currentURL = change.newValue
+                console.log("CurrentURL Value changed to: ", currentURL)
+                console.log("URL has been set in the chrome storage")
+                console.log("websites array has been set in the chrome storage")
+                console.log("table array has been set in the chrome storage")
+                trackHyperlinks();
+                // Listen for page updates (e.g., AJAX) and re-run the tracking function.
+                const observer = new MutationObserver(trackHyperlinks);
+                observer.observe(document.body, { childList: true, subtree: true });
+            } // Strict Target matching needed
+            
         }
     }
 });
@@ -32,7 +36,7 @@ function trackHyperlinks() {
     // Get the current data and then go through the system
 
     chrome.storage.local.get(
-        ["website", "table", "currentURL", "hyperlinks"],
+        ["website", "table", "currentURL", "hyperlink"],
         (res) => {
             if(chrome.runtime.error) {
                 console.log("Error has occured")
@@ -40,18 +44,36 @@ function trackHyperlinks() {
                 const website_local = res.website || []
                 const table_local = res.table || []
                 const hyperlink_local = res.hyperlink || []
+                
                 for(const link of linkData) {
                     const foundObject = table_local.find(obj => obj.website === res.currentURL);
                     if(foundObject) {
-                        if(foundObject.hyperlinks.includes(link)===false) {
-                            foundObject.hyperlinks.push(link)
-                            console.log("Hyperlink tagged to object")
+                        //TODO: Convert from link to an object
+                        const ObjectFinder = foundObject.hyperlinks.find(ob => ob.hyperlink === link)
+                        if(ObjectFinder) {
+
+                            // Object Exists, now you can incremenet the hyperlink
+                            ObjectFinder.occurence += 1
+
+                        } else {
+
+                            // Object not found and new object created and tagged
+                            holder = {
+                                hyperlink : link,
+                                occurence : 1
+                            }
+                            foundObject.hyperlinks.push(holder)
+                            console.log("Hyperlink tagged to object, with object instantiated")
+
                         }
                     }
+
+                    console.log("Table Logged: ", table_local)
+
                     if(hyperlink_local.includes(link)===false)  {
                         hyperlink_local.push(link)
                         console.log("Hyperlink added to the hyperlink list")
-                    }
+                    } 
                 }
 
                 // Set the data in the chrome storage
@@ -70,10 +92,8 @@ function trackHyperlinks() {
 
   }
   
-  trackHyperlinks();
+
   
-  // Listen for page updates (e.g., AJAX) and re-run the tracking function.
-  const observer = new MutationObserver(trackHyperlinks);
-  observer.observe(document.body, { childList: true, subtree: true });
+
 
 console.log(currentURL)
--- popup.js
+++ popup.js
@@ -2,6 +2,9 @@
 //Get the data from the popup.html
 const urlHolder = document.getElementById("url")
 
+const websiteCounter = document.getElementById("website-counter")
+const hyoerlinkCounter = document.getElementById("hyperlink-counter")
+
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   if (tabs.length > 0) {
@@ -10,13 +13,14 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       console.log("Current URL: " + currentURL)
 
 
-      chrome.storage.local.get(["website", "table"],
+      chrome.storage.local.get(["website", "table", "hyperlink"],
       (res) => {
         if(chrome.runtime.error) {
           console.log("Error has occured")
         } else  {
           const websites_local = res.website || []
           const table_local = res.table || []
+          const hyperlinks_local = res.hyperlink
 
 
           // Check if the website exists in array already
@@ -46,6 +50,11 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             console.log("table array has been set in the chrome storage")
             console.log(table_local)
             console.log(websites_local)
+
+            // Add the data to the webpage
+            websiteCounter.textContent = `The number of websites visited: ${websites_local.length}`
+            hyoerlinkCounter.textContent = `The number of hyperlinks encountered: ${hyperlinks_local.length}`
+
           })
 
         }
@@ -56,4 +65,14 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       
   }
 });
+
+chrome.storage.onChanged.addListener(function(changes, areaName) {
+  if(changes.website) {
+    websiteCounter.textContent = `The number of websites visited: ${changes.website.newValue.length}`
+  } else if(changes.hyperlink) {
+    hyoerlinkCounter.textContent = `The number of hyperlinks encountered: ${changes.hyperlink.newValue.length}`
+  }
+})
+
+
 
